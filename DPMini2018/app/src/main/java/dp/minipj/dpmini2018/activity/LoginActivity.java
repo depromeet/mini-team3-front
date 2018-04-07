7b1ce57554bdbb4db52cc6a2f8dd737d99619356
haelynn <estar2939@gmail.com>
2018-04-07 14:30:34 +0900

    login

--- DPMini2018/app/src/main/java/dp/minipj/dpmini2018/activity/LoginActivity.java
+++ DPMini2018/app/src/main/java/dp/minipj/dpmini2018/activity/LoginActivity.java
@@ -2,14 +2,32 @@ package dp.minipj.dpmini2018.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import dp.minipj.dpmini2018.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText email_et;
+    private EditText pw_et;
+    private ImageButton signIn_btn;
+    private ImageButton signUp_btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        /////
+        email_et = (EditText)findViewById(R.id.login_email_et);
+        pw_et = (EditText)findViewById(R.id.login_pw_et);
+        signIn_btn = (ImageButton)findViewById(R.id.login_btn);
+        signUp_btn = (ImageButton)findViewById(R.id.signup_btn);
+        /////
+
+        String userEmail = email_et.getText().toString();
+        String userPW = pw_et.getText().toString();
+
     }
 }
