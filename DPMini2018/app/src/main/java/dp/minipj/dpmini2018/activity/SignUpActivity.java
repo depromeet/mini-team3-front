2adaa6e724fb98f693df8e77d8adc354466cd161
Joseph.kim <joseph.kim@idincu.com>
2018-04-07 15:12:23 +0900

    signup 초안

--- DPMini2018/app/src/main/java/dp/minipj/dpmini2018/activity/SignUpActivity.java
+++ DPMini2018/app/src/main/java/dp/minipj/dpmini2018/activity/SignUpActivity.java
@@ -7,23 +7,27 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import dp.minipj.dpmini2018.R;
 
 public class SignUpActivity extends AppCompatActivity {
-
   @BindView(R.id.image_profile) ImageView imageProfile;
   @BindView(R.id.edit_emails) EditText editEmails;
   @BindView(R.id.edit_nickname) EditText editNickname;
   @BindView(R.id.edit_phone) EditText editPhone;
   @BindView(R.id.edit_password) EditText editPassword;
-  @BindView(R.id.button_signin) ImageView buttonSignin;
+  @BindView(R.id.button_signup) ImageView buttonSignup;
   @BindView(R.id.layout_button) RelativeLayout layoutButton;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_sign_up);
+    ButterKnife.bind(this);
+  }
 
+  @OnClick(R.id.button_signup) void onSignInClicked() {
 
   }
 }
---
+++ DPMini2018/app/src/main/java/dp/minipj/dpmini2018/models/SignUpResponse.java
@@ -0,0 +1,4 @@
+package dp.minipj.dpmini2018.models;
+
+public class SignUpResponse {
+}
