3cadb978b13421c54d8438e898129ca3a7382f25
haelynn <estar2939@gmail.com>
2018-04-07 16:44:52 +0900

    main

--- DPMini2018/app/src/main/java/dp/minipj/dpmini/activity/MainActivity.java
+++ DPMini2018/app/src/main/java/dp/minipj/dpmini/activity/MainActivity.java
@@ -3,9 +3,12 @@ package dp.minipj.dpmini.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.ImageButton;
 import android.widget.TextView;
+
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -22,11 +25,21 @@ public class MainActivity extends AppCompatActivity {
   @BindView(R.id.main_rv)
   RecyclerView mainRv;
 
+  private RecyclerView recyclerView;
+  // private RecyclerAdapter recyclerAdapter;
+  private LinearLayoutManager layoutManager;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     ButterKnife.bind(this);
+
+    ////// layout manager setting
+    recyclerView = (RecyclerView)findViewById(R.id.main_rv);
+    layoutManager = new LinearLayoutManager(this);
+    layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+    recyclerView.setLayoutManager(layoutManager);
 
   }
 
@@ -34,4 +47,16 @@ public class MainActivity extends AppCompatActivity {
     Intent intent = new Intent(activity, MainActivity.class);
     return Intent.makeRestartActivityTask(intent.getComponent());
   }
+
+  /*
+  class RecyclerAdapter extends RecyclerView.Adapter<MyViewHolder>{
+
+
+  }
+
+  class MyViewHolder extends RecyclerView.ViewHolder{
+
+  }
+  */
+
 }
---
+++ DPMini2018/app/src/main/java/dp/minipj/dpmini2018/models/MainResult.java
@@ -0,0 +1,10 @@
+package dp.minipj.dpmini2018.models;
+
+/**
+ * Created by lynn on 2018-04-07.
+ */
+
+public class MainResult {
+
+
+}
