ee709ecc850d3ef8da3179338f8be5ff1792f23b
Kyungman Kim <gracefulife@naver.com>
2018-04-07 16:04:44 +0900

    bind event

--- DPMini2018/app/src/main/java/dp/minipj/dpmini2018/activity/LoginActivity.java
+++ DPMini2018/app/src/main/java/dp/minipj/dpmini2018/activity/LoginActivity.java
@@ -9,28 +9,29 @@ import android.widget.RelativeLayout;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import dp.minipj.dpmini2018.R;
 
 public class LoginActivity extends AppCompatActivity {
-    @BindView(R.id.login_logo_img)
-    ImageView loginLogoImg;
-    @BindView(R.id.login_email_et)
-    EditText loginEmailEt;
-    @BindView(R.id.login_pw_et)
-    EditText loginPwEt;
-    @BindView(R.id.login_btn)
-    ImageButton loginBtn;
-    @BindView(R.id.login_content_layout)
-    RelativeLayout loginContentLayout;
-    @BindView(R.id.signup_btn)
-    ImageButton signupBtn;
+  @BindView(R.id.login_logo_img) ImageView loginLogoImg;
+  @BindView(R.id.login_email_et) EditText loginEmailEt;
+  @BindView(R.id.login_pw_et) EditText loginPwEt;
+  @BindView(R.id.login_btn) ImageButton loginBtn;
+  @BindView(R.id.login_content_layout) RelativeLayout loginContentLayout;
+  @BindView(R.id.signup_btn) ImageButton signupBtn;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        ButterKnife.bind(this);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_login);
+    ButterKnife.bind(this);
+  }
 
+  @OnClick(R.id.login_btn) void onLoginButtonClicked() {
+    startActivity(MainActivity.intent(this));
+  }
 
-    }
+  @OnClick(R.id.signup_btn) void onSignUpButtonClicked() {
+    startActivity(SignUpActivity.intent(this));
+  }
 }
